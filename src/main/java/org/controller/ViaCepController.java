79dec787e26b4ec85197f1d1e3df712a11176bea
juliano <julianoosmir@outlook.com>
2022-03-11 20:09:55 -0400

    Configure the HTTP Port.

--- src/main/java/org/controller/ViaCepController.java
+++ src/main/java/org/controller/ViaCepController.java
@@ -2,14 +2,13 @@ package org.controller;
 
 import org.eclipse.microprofile.rest.client.inject.RestClient;
 import org.service.ViaCepService;
+
 import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
-
 
 @Path("/viacep")
 public class ViaCepController {
@@ -20,9 +19,8 @@ public class ViaCepController {
 
     @GET
     @Produces(MediaType.APPLICATION_JSON)
-    public Response getCep(@QueryParam("cep") String cep){
-        Response reps = viaCepService.getCep(cep);
-        return reps;
+    public Object getCep(@QueryParam("cep") String cep) {
+        return viaCepService.getCep(cep);
     }
 
 }
